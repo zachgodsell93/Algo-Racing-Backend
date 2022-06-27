fdbb1b03c9b28955834ebc709620863925f3f09c
Zach Godsell <zach@zachgodsell.com>
2022-06-28 07:44:20 +1000

    update

--- results.py
+++ results.py
@@ -4,7 +4,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 # Use a service account
-cred = credentials.Certificate('home/zachgodsell/ar-backend/algoracing.json')
+cred = credentials.Certificate('/home/zachgodsell/ar-backend/algoracing.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
